d9b31b051a245961aef5956a071a821caa75cc19
Alma monika <61734592+Almamonika07@users.noreply.github.com>
2020-07-06 10:44:22 +0800

    Add files via upload

---
+++ IPrinterWindows.cpp
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismInterface
+{
+    interface IPrinterWindows
+    {
+        void Show();
+        void Print();
+    }
